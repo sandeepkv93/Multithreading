b9bc847c24924b1c390083fe41fc702ec76a8b48
sandeepkv <sandeepkv93@gmail.com>
2016-11-16 11:35:21 +0530

    Readers Writers Using Semaphore

---
+++ Multithreading/src/customprinter/AlternatePrinter.java
@@ -0,0 +1,16 @@
+package customprinter;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class AlternatePrinter {
+	private List<Character> list1 = new LinkedList<Character>();
+	private List<Character> list2 = new LinkedList<Character>();
+	private Object lock = new Object();
+	public void printListOne() throws InterruptedException {
+		int value = 0;
+		while(true) {
+			/* Incomplete Code*/
+		}
+	}
+}
--- Multithreading/src/producerconsumerbysemaphores/Processor.java
+++ Multithreading/src/producerconsumerbysemaphores/Processor.java
@@ -1,7 +1,8 @@
-package producerconsumerbylocks;
+package producerconsumerbysemaphores;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -9,42 +10,32 @@ import java.util.concurrent.locks.ReentrantLock;
 public class Processor {
 	private static final int LIMIT = 10;
 	private List<Integer> list = new LinkedList<Integer>();
-	private Lock myLock = new ReentrantLock();
-	private Condition condition = myLock.newCondition();
+	Semaphore semaphoreProducer=new Semaphore(1);
+    Semaphore semaphoreConsumer=new Semaphore(0);
 	public void produce() throws InterruptedException {
 		int value = 0;
 		while(true) {
-			myLock.lock();
-			try {
 				while (list.size() == LIMIT) {
-					condition.await();
+					semaphoreProducer.acquire();
 				}
 				System.out.print("List Size: "+list.size());
 				value = value+1;
 				System.out.println("; Producer Produced Value: "+value);
 				list.add(value);
-				condition.signal();
-			} finally {
-				myLock.unlock();
-			}
+				semaphoreConsumer.release();
 		}
 	}
 	
 	public void consume() throws InterruptedException {
 		while(true) {
-			myLock.lock();
-			try {
-				while (list.size() == 0) {
-					condition.await();
+				while (list.isEmpty()) {
+					semaphoreConsumer.acquire();
 				}
 				System.out.print("List Size: "+list.size());
 				int value = list.remove(0);
 				System.out.println("; Consumer Consumed Value: "+value);
-				condition.signal();
-			} finally {
-				myLock.unlock();
-			}
-			//Thread.sleep(1000);
+				semaphoreProducer.release();
+				Thread.sleep(1000);
 		}
 	}
 	
