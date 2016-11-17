4517d33d19a60e07f821a9e2bf23a0df5352a55d
sandeepkv <sandeepkv93@gmail.com>
2016-11-17 13:27:00 +0530

    Alternate Printer Using 2 Threads

--- Multithreading/src/customprinter/AlternatePrinter.java
+++ Multithreading/src/customprinter/AlternatePrinter.java
@@ -1,16 +1,34 @@
 package customprinter;
 
-import java.util.LinkedList;
 import java.util.List;
 
 public class AlternatePrinter {
-	private List<Character> list1 = new LinkedList<Character>();
-	private List<Character> list2 = new LinkedList<Character>();
+	private Integer count = new Integer(0);
 	private Object lock = new Object();
-	public void printListOne() throws InterruptedException {
-		int value = 0;
-		while(true) {
-			/* Incomplete Code*/
+	public void printEven() throws InterruptedException {
+		while(count <= 100) {
+			synchronized (lock) {
+				while(count%2 != 0) {
+					lock.wait();
+				}
+				System.out.println("Even Thread: "+count);
+				count = count+1;
+				lock.notify();
+				Thread.sleep(100);
+			}
+		}
+	}
+	public void printOdd() throws InterruptedException {
+		while(count <= 100) {
+			synchronized (lock) {
+				while(count%2 == 0) {
+					lock.wait();
+				}
+				System.out.println("Odd Thread: "+count);
+				count = count+1;
+				lock.notify();
+				Thread.sleep(100);
+			}
 		}
 	}
 }
